bdd48850e39e6baaed72ac82d59b96a31697e1a0
vaneshita <144995@unsaac.edu.pe>
2018-02-20 09:27:56 -0500

    inicializando firebase

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+// Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyDqeJqH3_aM_vSYtvj2n8c9XIl6CMrvpAw',
+    authDomain: 'fare-estimate-4c274.firebaseapp.com',
+    databaseURL: 'https://fare-estimate-4c274.firebaseio.com',
+    projectId: 'fare-estimate-4c274',
+    storageBucket: '',
+    messagingSenderId: '1045327683760'
+  };
+  firebase.initializeApp(config);
+  alert('hola');
+});
