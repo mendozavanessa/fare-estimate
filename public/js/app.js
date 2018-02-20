a4edae35a72cad7784f1b9fd06427d5f0ae283e9
tayrih <tahysluana@gmail.com>
2018-02-20 12:29:54 -0500

    inicializa google maps

--- public/js/app.js
+++ public/js/app.js
@@ -10,9 +10,48 @@ $(document).ready(function() {
   };
   firebase.initializeApp(config);
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.uber.com/v1.2/products?latitude=37.7759792&longitude=-122.41823');
-  xhr.setRequestHeader('Authorization', 'FXxrk_qIXvnWx5oKah7QxHeM5zp_D_doiYA9nf2E');
-  xhr.send();
+  // var xhr = new XMLHttpRequest();
+  // xhr.open('GET', 'https://api.uber.com/v1.2/products?latitude=37.7759792&longitude=-122.41823');
+  // xhr.setRequestHeader('Authorization', 'FXxrk_qIXvnWx5oKah7QxHeM5zp_D_doiYA9nf2E');
+  // xhr.send();
 
 });
+
+
+// init Map
+
+var map;
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397,
+      lng: 150.644},
+    zoom: 17
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+infoWindow.setPosition(pos);
+infoWindow.setContent(browserHasGeolocation ?
+  'Error: The Geolocation service failed.' :
+  'Error: Your browser doesn\'t support geolocation.');
+}
