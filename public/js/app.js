4fa69f0c9415d70e308b652285082dc9c31254f8
vaneshita <144995@unsaac.edu.pe>
2018-02-20 12:03:18 -0500

    agregando framework en index.html

--- public/js/app.js
+++ public/js/app.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
     messagingSenderId: '1045327683760'
   };
   firebase.initializeApp(config);
-  alert('hola');
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.uber.com/v1.2/products?latitude=37.7759792&longitude=-122.41823');
+  xhr.setRequestHeader('Authorization', 'FXxrk_qIXvnWx5oKah7QxHeM5zp_D_doiYA9nf2E');
+  xhr.send();
+
 });
