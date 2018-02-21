f8c8ca831b2bba4b35cb1d85abd0cd7addc208bc
Cintia Tahirih <tahysluana@gmail.com>
2018-02-20 23:53:31 -0500

    agrega mapa

--- public/js/app.js
+++ public/js/app.js
@@ -90,12 +90,12 @@ window.addEventListener('load', function() {
 });
 
 var config = {
-  apiKey: "AIzaSyCnvceLHFJR5Q5Y_G89_bc_dJH-q0Hx8bs",
-  authDomain: "fare-estimate-tv.firebaseapp.com",
-  databaseURL: "https://fare-estimate-tv.firebaseio.com",
-  projectId: "fare-estimate-tv",
-  storageBucket: "",
-  messagingSenderId: "25625867728"
+  apiKey: 'AIzaSyCnvceLHFJR5Q5Y_G89_bc_dJH-q0Hx8bs',
+  authDomain: 'fare-estimate-tv.firebaseapp.com',
+  databaseURL: 'https://fare-estimate-tv.firebaseio.com',
+  projectId: 'fare-estimate-tv',
+  storageBucket: '',
+  messagingSenderId: '25625867728'
 };
 firebase.initializeApp(config);
 var userName = $('#nameUser');
